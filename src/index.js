c217bb8aa48e36fabf8d340f9002d2041483ed24
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-04-06 17:45:02 +0900

    featL bootstarp

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ import HelloWorld from "./HelloWorld";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppClass />
-    <HelloWorld />
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <AppClass />
+          <HelloWorld />
+        </div>
+      </div>
+    </div>
   </React.StrictMode>,
 );
