d6324f0021b2c7246d7cca2022e1d139d792e99f
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-04-06 17:35:36 +0900

    hello world functional component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppClass from "./AppClass";
+import HelloWorld from "./HelloWorld";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppClass />
+    <HelloWorld />
   </React.StrictMode>,
 );
