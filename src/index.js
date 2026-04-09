f318c4f579bf398197b8d46da1b4018c8a5ed4fe
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-04-09 12:00:42 +0900

    react router セットアップ

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <App />
-        </div>
-      </div>
-    </div>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
