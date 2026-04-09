796f8c7bcbbe1333fc728f7c897e3ceece4c8eed
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-04-10 00:32:53 +0900

    ログイン画面の作成

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,37 @@
+import { useState } from "react";
+import Input from "./form/Input";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <div>
+    <div className="col-md-6 offset-md-3">
       <h2>Login</h2>
       <hr />
+      <form onSubmit={handleSubmit}>
+        <Input
+          title="Email Address"
+          type="email"
+          className="form-control"
+          name="email"
+          autoComplete="email-new"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Input
+          title="Password"
+          type="password"
+          className="form-control"
+          name="password"
+          autoComplete="password-new"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </form>
     </div>
   );
 };
