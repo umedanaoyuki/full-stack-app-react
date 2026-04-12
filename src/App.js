777fb3308bfa6fc42680b347359c1f3899febd2e
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2026-04-12 17:44:37 +0900

    feat: ログアウト機能実装

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import Alert from "./components/Alert";
 
 function App() {
   const [jwtToken, setJwtToken] = useState("");
   const [alertMessage, setAlertMessage] = useState("");
   const [alertClassName, setAlertClassName] = useState("d-none");
+
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    setJwtToken("");
+    navigate("/login");
+  };
 
   return (
     <div className="container">
@@ -19,7 +26,7 @@ function App() {
               <span className="badge bg-success">Login</span>
             </Link>
           ) : (
-            <a href="/#">
+            <a href="#!" onClick={logOut}>
               <span className="badge bg-danger">Logout</span>
             </a>
           )}
